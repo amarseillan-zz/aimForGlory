dbf5832f0e245ed181f11b30c89de383fab7b093
Agustin Marseillan <agustinmarseillan@gmail.com>
2013-09-12 20:02:01 -0300

    Initial Commit

---
+++ PhantomAssasins/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  PhantomAssasins
+//
+//  Created by Agustin Marseillan on 9/12/13.
+//  Copyright Agustin Marseillan 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ PhantomAssasins/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PhantomAssasins
+//
+//  Created by Agustin Marseillan on 9/12/13.
+//  Copyright Agustin Marseillan 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
